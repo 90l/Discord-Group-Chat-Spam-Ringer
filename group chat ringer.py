8108eaf4bd9abebe1fa01c9b467922f25183aa3b
BLK <78625896+90l@users.noreply.github.com>
2022-01-17 23:59:08 -0500

    user input with requests

--- group chat ringer.py
+++ group chat ringer.py
@@ -7,13 +7,14 @@ with open('config.json') as f:
 
 token = config.get('token')
 
+amounts = int(input("How many requests do you want to make? \n"))
 
-while True:
-    global cycling
-    cycling = True
-    while cycling:
-      headers = {
-          'Authorization': token,
-          'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) discord/1.0.1011 Chrome/91.0.4472.164 Electron/13.4.0 Safari/537.36'
-      }
-      requests.post('https://ptb.discord.com/api/v9/channels/927854968126451732/call/ring', headers=headers, json={"recipients":["913866260536762430","775637384812822528"]})
+
+for _ in range(amounts):
+            headers = {
+            'Authorization': token,
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) discord/1.0.1011 Chrome/91.0.4472.164 Electron/13.4.0 Safari/537.36'
+            }
+            requests.post('https://discord.com/api/v9/channels/923366115038617630/call/ring', headers=headers, json={"recipients":["913866260536762430","775637384812822528"]})
+
+
